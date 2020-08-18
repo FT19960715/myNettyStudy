01fe49acee2c0855c201e5817962482a659f7f71
fengtao <fengtao@163.com>
2020-08-18 08:08:18 +0800

    first commit    init project

--- src/main/java/com/Netty/NIO/NioServer/NIOServer.java
+++ src/main/java/com/Netty/NIO/NioServer/NIOServer.java
@@ -12,23 +12,23 @@ public class NIOServer {
         // 创建ServerSocketChannel
         ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
         // 得到一个Selector对象
-        Selector open = Selector.open();
+        Selector selector = Selector.open();
         // 绑定一个端口
         serverSocketChannel.socket().bind(new InetSocketAddress("127.0.0.1",6666));
         // 设置为非阻塞
         serverSocketChannel.configureBlocking(false);
         // 将ServerSocktChannel 注册打Selector， 关心事件为 OP_ACCEPT
-        serverSocketChannel.register(open, SelectionKey.OP_ACCEPT);
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
         // 循环等待客户端链接
         while(true){
             // 这里阻塞五秒钟，如果五秒中无时间发生
-            if(open.select(1000) == 0){
+            if(selector.select(1000) == 0){
                 System.out.println("z暂时无事件发生");
                 // 暂时没有任何事件发生
             }else{
                 // 如果返回大于零,获取到相关的selectKey集合，有事件发生的selectKey
                 // 通过selectKey可以获取到相关的SocketChannel
-                Set<SelectionKey> selectionKeys = open.selectedKeys();
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
                 Iterator<SelectionKey> iterator = selectionKeys.iterator();
                 while(iterator.hasNext()){
                     SelectionKey e = iterator.next();
@@ -39,7 +39,7 @@ public class NIOServer {
                             // 将产生的socketChannel指定为非阻塞模型
                             socketChannel.configureBlocking(false);
                             // 将当前的socketChannel也注册到Selector上,关注事件为OP_READ，同时为该socketChannel关联一个Buffer
-                            socketChannel.register(open,SelectionKey.OP_READ, ByteBuffer.allocate(1024));
+                            socketChannel.register(selector,SelectionKey.OP_READ, ByteBuffer.allocate(1024));
                     }else if(e.isReadable()){
                         // 从客户端读取数据，发生了 OP_READ
                         // 通过key反向获取到对应的Channel
@@ -58,7 +58,7 @@ public class NIOServer {
 //                        try {
 //                            SocketChannel socketChannel = serverSocketChannel.accept();
 //                            // 将当前的socketChannel也注册到Selector上,关注事件为OP_READ，同时为该socketChannel关联一个Buffer
-//                            socketChannel.register(open,SelectionKey.OP_READ, ByteBuffer.allocate(1024));
+//                            socketChannel.register(selector,SelectionKey.OP_READ, ByteBuffer.allocate(1024));
 //                        } catch (IOException e1) {
 //                            e1.printStackTrace();
 //                        }
